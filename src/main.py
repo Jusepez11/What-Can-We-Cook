3fe683feee173635647d587a3db3ab47de655f13
Sebastian <jusepez90@gmail.com>
2025-09-02 16:09:46 -0400

    updated secret

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("secrets/testspreadsheet-257500-firebase-adminsdk-8dlsm-1288dc11ca.json")
+cred = credentials.Certificate("secrets/secret.json")
 firebase_admin.initialize_app(cred)
