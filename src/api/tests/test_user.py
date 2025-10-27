73961c4560df0b048425d9d802cf419334f8450d
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-10-26 20:19:59 -0400

    Update test_user.py

--- src/api/tests/test_user.py
+++ src/api/tests/test_user.py
@@ -3,14 +3,14 @@ def test_create_user(client, test_seed_data, authenticate_demo_user):
 		"name": "Juan"
 	}
 
-	response = client.post("/user", json=new_user, headers=authenticate_demo_user)
+	response = client.post("/users", json=new_user, headers=authenticate_demo_user)
 	assert response.status_code == 200
 	data = response.json()
 	assert data["name"] == new_user["name"]
 
 
 def test_get_users(client, test_seed_data):
-	response = client.get("/user")
+	response = client.get("/users")
 	assert response.status_code == 200
 	data = response.json()
 	assert isinstance(data, list)
@@ -19,7 +19,7 @@ def test_get_users(client, test_seed_data):
 
 
 def test_search_users(client, test_seed_data):
-	response = client.get("/user/search/", params={"query": "Marian", "threshold": 60})
+	response = client.get("/users/search/", params={"query": "Marian", "threshold": 60})
 	assert response.status_code == 200
 	data = response.json()
 	assert isinstance(data, list)
@@ -31,15 +31,15 @@ def test_update_user(client, test_seed_data, authenticate_demo_user):
 		"name": "Updated Mariana"
 	}
 
-	response = client.put(f"/user/1", json=updated_user, headers=authenticate_demo_user)
+	response = client.put(f"/users/1", json=updated_user, headers=authenticate_demo_user)
 	assert response.status_code == 200
 	data = response.json()
 	assert data["name"] == updated_user["name"]
 
 
 def test_delete_user(client, test_seed_data, authenticate_demo_user):
-	response = client.delete(f"/user/1", headers=authenticate_demo_user)
+	response = client.delete(f"/users/1", headers=authenticate_demo_user)
 	assert response.status_code == 200
 
-	response = client.get(f"/user/1")
+	response = client.get(f"/users/1")
 	assert response.status_code == 404
