c1c1aaedf2c3fde1610fedd3f79eea7e7f2be700
Nicholas Smit <mcgamewarrior@gmail.com>
2025-10-26 23:32:43 -0400

    Fix broken user test
    Fix broken delete user test throwing 401s in some scenarios due to the previous update user test changing JWT tokens.

--- src/api/tests/test_recipe.py
+++ src/api/tests/test_recipe.py
@@ -1,28 +1,28 @@
 def test_get_recipe(client, test_seed_data):
-    response = client.get("/recipes/1")
-    assert response.status_code == 200
-    data = response.json()
-    assert data["id"] == 1
-    assert data["title"] == "Kapsalon"
+	response = client.get("/recipes/1")
+	assert response.status_code == 200
+	data = response.json()
+	assert data["id"] == 1
+	assert data["title"] == "Kapsalon"
 
 
 def test_update_recipe(client, test_seed_data, authenticate_demo_user):
-    updated_recipe = {
-		"title": "Updated Test Recipe"}
+	updated_recipe = {
+		"title": "Updated Test Recipe"
+	}
 
-
-    response = client.put("/recipes/1", json=updated_recipe, headers=authenticate_demo_user)
-    assert response.status_code == 200
-    data = response.json()
-    assert data["title"] == updated_recipe["title"]
+	response = client.put("/recipes/1", json=updated_recipe, headers=authenticate_demo_user)
+	assert response.status_code == 200
+	data = response.json()
+	assert data["title"] == updated_recipe["title"]
 
 
 def test_delete_recipe(client, test_seed_data, authenticate_demo_user, authenticate_demo_admin_user):
-    response = client.delete("/recipes/1", headers=authenticate_demo_user)
-    assert response.status_code == 401  # Non-admin recipe should not be able to delete
+	response = client.delete("/recipes/1", headers=authenticate_demo_user)
+	assert response.status_code == 403  # Non-admin recipe should not be able to delete
 
-    response = client.delete("/recipes/1", headers=authenticate_demo_admin_user)
-    assert response.status_code == 204  # Admin recipe should be able to delete
+	response = client.delete("/recipes/1", headers=authenticate_demo_admin_user)
+	assert response.status_code == 204  # Admin recipe should be able to delete
 
-    response = client.get("/recipes/1")
-    assert response.status_code == 404
+	response = client.get("/recipes/1")
+	assert response.status_code == 404
--- src/api/tests/test_user.py
+++ src/api/tests/test_user.py
@@ -8,18 +8,18 @@ def test_get_user(client, test_seed_data):
 
 def test_update_user(client, test_seed_data, authenticate_demo_user):
 	updated_user = {
-		"username": "Updated Test User"
+		"email": "updated@mail.com"
 	}
 
 	response = client.put("/users/1", json=updated_user, headers=authenticate_demo_user)
 	assert response.status_code == 200
 	data = response.json()
-	assert data["username"] == updated_user["username"]
+	assert data["email"] == updated_user["email"]
 
 
 def test_delete_user(client, test_seed_data, authenticate_demo_user, authenticate_demo_admin_user):
-	response = client.delete(f"/users/1", headers=authenticate_demo_user)
-	assert response.status_code == 401  # Non-admin user should not be able to delete
+	response = client.delete("/users/1", headers=authenticate_demo_user)
+	assert response.status_code == 403  # Non-admin user should not be able to delete
 
 	response = client.delete("/users/1", headers=authenticate_demo_admin_user)
 	assert response.status_code == 204  # Admin user should be able to delete
